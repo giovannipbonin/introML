d6e39b98adde4696df68566c7757d011cce98f3e
Giovanni Pascotto Bonin <pbonin.giovanni@gmail.com>
2016-04-19 18:01:16 -0400

    implement decision tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,6 +25,16 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+import sklearn.tree as tree
+import sklearn.metrics as metrics
+print len(features_train[0])
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+print metrics.accuracy_score(pred, labels_test)
+
+
 
 #########################################################
 
