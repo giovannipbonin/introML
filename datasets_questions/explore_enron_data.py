ce489791c1d91295acde2c1f0e14fc0dce234698
Giovanni Pascotto Bonin <pbonin.giovanni@gmail.com>
2016-04-23 17:50:55 -0400

    complete explore_enron_data.py

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -12,11 +12,53 @@
     but here's an example to get you started:
 
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
 """
 
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+# Number of datapoints
+print len(enron_data.keys())
 
+# Number of features
+print len(enron_data[enron_data.keys()[0]])
+
+# Number of POIs in dataset
+count = 0
+for k in enron_data.keys():
+    if enron_data[k]["poi"]: count += 1
+print count
+
+# James Prentice Stock
+
+print enron_data["PRENTICE JAMES"]["total_stock_value"]
+
+# Messages from Wesley Colwell
+
+print enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+
+# Stock options exercised by Jeff SKILLING
+
+print enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+
+# Who took most money
+
+print enron_data["SKILLING JEFFREY K"]["total_payments"]
+print enron_data["LAY KENNETH L"]["total_payments"]
+print enron_data["FASTOW ANDREW S"]["total_payments"]
+
+# Non NaN salary and email, and NaN total payments, and POIs without financial data
+
+countSalary = 0
+countEmail = 0
+countTotalPayments = 0
+countNanPOIs = 0
+
+for k in enron_data.keys():
+    if enron_data[k]['salary'] != 'NaN': countSalary += 1
+    if enron_data[k]['email_address'] != 'NaN': countEmail += 1
+    if enron_data[k]['total_payments'] == 'NaN': countTotalPayments += 1
+    if (enron_data[k]['poi'] and enron_data[k]['total_payments'] == 'NaN'): countNanPOIs += 1
+
+print countSalary, countEmail, countTotalPayments, countNanPOIs
